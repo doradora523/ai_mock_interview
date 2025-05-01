44c4b1192077ca5c80f6703aaea753e3ae136002
123 <a123@123ui-MacBookPro.local>
2025-05-02 01:32:48 +0900

    ignore eslint and typescript errors (for now)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -66,24 +66,6 @@ async function Home() {
             <section className="flex flex-col gap-6 mt-8">
                 <h2>Take Interviews</h2>
 
-                {/* <div className="interviews-section">
-                    {hasUpcomingInterviews ? (
-                        allInterview?.map((interview) => (
-                            <InterviewCard
-                                key={interview.id}
-                                userId={user?.id}
-                                interviewId={interview.id}
-                                role={interview.role}
-                                type={interview.type}
-                                techstack={interview.techstack}
-                                createdAt={interview.createdAt}
-                            />
-                        ))
-                    ) : (
-                        <p>There are no interviews available</p>
-                    )}
-                </div> */}
-            </section>
         </>
     );
 }
